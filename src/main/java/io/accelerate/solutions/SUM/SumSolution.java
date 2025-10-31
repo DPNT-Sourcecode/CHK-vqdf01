35334c688b90e22db4be69730fa9a4731258d4d5
sbx_user1051 <sbx_user1051@169.254.12.89>
2025-10-31 23:19:32 +0000

    Fri Oct 31 23:19:32 UTC 2025

--- src/main/java/io/accelerate/solutions/SUM/SumSolution.java
+++ src/main/java/io/accelerate/solutions/SUM/SumSolution.java
@@ -6,8 +6,11 @@ import io.accelerate.runner.SolutionNotImplementedException;
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new IllegalArgumentException("Both params should be positive int between 0-100");
+        if(x<0||x>100||y<0||y>100)
+            throw new IllegalArgumentException("Both params should be positive int between 0-100");
+        return x+y;
     }
 
 }
+
 
