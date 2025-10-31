2e6bd6b231ed127e0722f359c3db87729a98a4e0
sbx_user1051 <sbx_user1051@169.254.12.89>
2025-10-31 23:28:41 +0000

    Fri Oct 31 23:28:41 UTC 2025

--- src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
+++ src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
@@ -1,9 +1,15 @@
 package io.accelerate.solutions.HLO;
-
+import java.util.Objects;
 import io.accelerate.runner.SolutionNotImplementedException;
+
 
 public class HelloSolution {
     public String hello(String friendName) {
-        throw new SolutionNotImplementedException();
+        Objects.requireNonNull(friendName,"friendName cannot be null");
+        if(friendName.trim().isEmpty()) {
+            throw new IllegalArgumentException("friendName cannot be empty");
+        }
+        return "Hello, " + friendName;
     }
 }
+
