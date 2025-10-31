ce3dcf5dc3d511e9f3c21814037f879688044f6a
sbx_user1051 <sbx_user1051@169.254.12.89>
2025-10-31 23:53:43 +0000

    Fri Oct 31 23:53:43 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -7,11 +7,18 @@ import java.util.Map;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        Map<Character,Integer> UNIT_PRICE = new HashMap<Character,Integer>();
+        Map<Character,Integer> unit_price = new HashMap<Character,Integer>();
+        unit_price.put('A',50);
+        unit_price.put('B',30);
+        unit_price.put('C',20);
+        unit_price.put('D',15);
+
+
         Map<Character,Integer> SPECIAL_OFFER = new HashMap<Character,Integer>();
 
 
         throw new SolutionNotImplementedException();
     }
 }
+
 
