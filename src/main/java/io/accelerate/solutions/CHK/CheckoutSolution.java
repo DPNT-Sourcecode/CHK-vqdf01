7b5141d1aa21fe993ce4a55f45c6494821949905
sbx_user1051 <sbx_user1051@169.254.12.89>
2025-10-31 23:56:43 +0000

    Fri Oct 31 23:56:43 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -13,12 +13,28 @@ public class CheckoutSolution {
         unit_price.put('C',20);
         unit_price.put('D',15);
 
+        //concludes the special offer table
+        Map<Character,Integer> quantity = new HashMap<Character,Integer>();
+        Map<Character,Integer> offer_price = new HashMap<Character,Integer>();
+        quantity.put('A',3);
+        offer_price.put('A',130);
+        quantity.put('B',2);
+        offer_price.put('B',45);
 
-        Map<Character,Integer> SPECIAL_OFFER = new HashMap<Character,Integer>();
+        //getting the count now
+        Map<Character, Integer> counts = new HashMap<>();
+        for (Character c : skus.toCharArray()) {
+            if(!unit_price.containsKey(c)){
+                return -1; //not throwing any Exception as it was mentioned to just retun -1
+            }
+        }
+
+
 
 
         throw new SolutionNotImplementedException();
     }
 }
+
 
 
