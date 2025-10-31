c3d9ddfd865b4dbfef99040ab2a642e44debe7c4
sbx_user1051 <sbx_user1051@169.254.12.89>
2025-10-31 23:44:43 +0000

    Fri Oct 31 23:44:43 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,14 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
+    private static final Map<Character,Integer> CHAR_MAP = new HashMap<Character,Integer>();
     public Integer checkout(String skus) {
+
+
         throw new SolutionNotImplementedException();
     }
 }
